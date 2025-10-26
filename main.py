b1f9677c5c111128fda6af560e4c822ae75e7e94
548821 <3270358320@qq.com>
2025-10-26 15:50:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def isOdd(value):
-    if isinstance(value,int) and not isinstance(value,bool):
-        return value%2!=0
-    else:
+def is_prime(num):
+    if num < 2:
         return False
+    if num == 2:
+        return True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def PrimeList(N):
+    primes = []
+    for num in range(2, N):
+        if is_prime(num):
+            primes.append(str(num))
+    return ' '.join(primes)
+if __name__ == "__main__":
+    print(PrimeList(10))
